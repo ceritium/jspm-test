7ed6194bfbc1057482ce131067c62b8964335f7d
Jose Galisteo <ceritium@gmail.com>
2015-06-10 16:01:54 +0200

    tests with karma, mocha and sinon-chai

---
+++ test/testSpec.js
@@ -0,0 +1,11 @@
+/* jshint -W030 */
+'use strict';
+
+import RedditApi from '../lib/reddit-api';
+
+describe('A test suite', function() {
+
+  it('should fail', function() {
+    expect(RedditApi).to.not.be.undefined;
+  });
+});
